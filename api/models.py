29b259d73c651aadb3e75c996a015c0f57417fdc
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-05-28 11:32:51 +0500

    init

--- api/models.py
+++ api/models.py
@@ -5,4 +5,4 @@ class Lesson(models.Model):
     name = models.CharField(max_length=70)
     file = models.FileField(upload_to='files/',null=True, blank=True)
     description = models.TextField()
-    users = models.ManyToManyField(User, related_name='lessons')
+    users = models.ManyToManyField(User)
