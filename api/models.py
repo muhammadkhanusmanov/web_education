40a4e95054274d6ddf13eab5130a0ef3aa4f0b94
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-05-28 07:50:01 +0500

    create model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lesson(models.Model):
+    name = models.CharField(max_length=70)
+    file = models.FileField(upload_to='files/',null=True, blank=True)
+    description = models.TextField()
+    users = models.ManyToManyField(User, related_name='lessons')
